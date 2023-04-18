98a1346b870fc760078cdc35129ba03c99e17c68
Andrej Kanuch <kanuchandrej@gmail.com>
2023-04-18 15:29:31 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let todos = [];
+
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.post("/todos", (req, res) => {
+  const newTodo = { id: Date.now(), text: req.body.text, completed: false };
+  todos.push(newTodo);
+  res.json(newTodo);
+});
+
+app.put("/todos/:id", (req, res) => {
+  const todo = todos.find((t) => t.id === parseInt(req.params.id));
+  if (!todo) return res.status(404).send("Not found");
+  todo.completed = !todo.completed;
+  res.json(todo);
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const index = todos.findIndex((t) => t.id === parseInt(req.params.id));
+  if (index === -1) return res.status(404).send("Not found");
+  todos.splice(index, 1);
+  res.sendStatus(204);
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
